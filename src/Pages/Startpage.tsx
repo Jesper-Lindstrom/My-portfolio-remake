2e7997b4d090e018fb4e0330044764f88af9e3bb
Jesper Lindström <108944899+Jesper-Lindstrom@users.noreply.github.com>
2023-05-18 20:40:25 +0200

    setup react router

---
+++ src/Pages/Startpage.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import About from "../Components/About";
+import AboutProjects from "../Components/AboutProjects";
+
+export default function Startpage() {
+  return (
+    <Box>
+      <About />
+      <AboutProjects />
+    </Box>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import App from "./App";
+import About from "./Components/About";
+import AboutProjects from "./Components/AboutProjects";
+import Projects from "./Pages/Projects";
+import Startpage from "./Pages/Startpage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Startpage />} />
+      <Route path="about" element={<About />}>
+        <Route path="projects" element={<AboutProjects />} />
+      </Route>
+      <Route path="projects" element={<Projects />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
- 
