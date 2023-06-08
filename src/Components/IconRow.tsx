942bbda099a4747e5653799caf74875438e0ff36
Jesper Lindström <108944899+Jesper-Lindstrom@users.noreply.github.com>
2023-06-08 12:22:38 +0200

    trying a bit of everything

--- src/Components/IconRow.tsx
+++ src/Components/IconRow.tsx
@@ -6,7 +6,7 @@ export function IconRow() {
       <Box sx={{ width: "2rem", height: "2rem", padding: "0 0.4rem" }}>
         <a href="https://www.linkedin.com/in/jesperlindstrom33/">
           <img
-            src="./images/linkedin-logo.png"
+            src="/linkedin-logo.png"
             alt="linked in link"
             style={{ width: "100%", height: "100%" }}
           />
@@ -14,20 +14,12 @@ export function IconRow() {
       </Box>
       <Box sx={{ width: "2rem", height: "2rem", padding: "0 0.4rem" }}>
         <a href="mailto:jesperlindstrom10@gmail.com">
-          <img
-            src="./images/mail-logo.png"
-            alt="mail me link"
-            style={{ width: "100%", height: "100%" }}
-          />
+          <img src="/mail-logo.png" alt="mail me link" style={{ width: "100%", height: "100%" }} />
         </a>
       </Box>
       <Box sx={{ width: "2rem", height: "2rem", padding: "0 0.4rem" }}>
         <a href="https://github.com/Jesper-Lindstrom">
-          <img
-            src="./images/github-logo.png"
-            alt="github link"
-            style={{ width: "100%", height: "100%" }}
-          />
+          <img src="/github-logo.png" alt="github link" style={{ width: "100%", height: "100%" }} />
         </a>
       </Box>
     </Box>
